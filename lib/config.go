171cc4dc7723d8634f19b820a84e3cb2955295c5
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-07-26 22:40:41 +0900

    feat: allow to configure application by environment variables

--- lib/config.go
+++ lib/config.go
@@ -3,10 +3,29 @@ package lib
 import (
 	"github.com/BurntSushi/toml"
 	"github.com/suzuki-shunsuke/japanese-holiday-api/types"
+	"os"
+	"strconv"
 )
 
 func GetConfig() (*types.Config, error) {
 	var config types.Config
 	_, err := toml.DecodeFile("config.toml", &config)
+	password := os.Getenv("RDB_PASSWORD")
+	host := os.Getenv("RDB_HOST")
+	dbName := os.Getenv("RDB_DBNAME")
+	port_str := os.Getenv("RDB_PORT")
+	if len(password) > 0 {
+		config.RDB.Password = password
+	}
+	if len(host) > 0 {
+		config.RDB.Host = host
+	}
+	if len(dbName) > 0 {
+		config.RDB.DBName = dbName
+	}
+	if len(port_str) > 0 {
+		config.RDB.Port, err = strconv.Atoi(port_str)
+	}
+
 	return &config, err
 }
