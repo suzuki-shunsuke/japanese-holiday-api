fc05556fb1db01dee45ea7167ec21e3e824eec00
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2017-07-30 12:56:20 +0900

    feat: 3.その前日及び翌日が「国民の祝日」である日（「国民の祝日」でない日に限る。）は、休日とする。
    http://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html

--- lib/handlers.go
+++ lib/handlers.go
@@ -19,15 +19,26 @@ func GetHolidays(c echo.Context) error {
 	db.Select("name, type, date, day_of_week").Find(&holidays_)
 	var startDate time.Time
 	var endDate time.Time
+	var prev_holiday time.Time
 	for i, h := range holidays_ {
 		if i == 0 {
 			startDate = time.Date(h.Date.Year(), 1, 1, 0, 0, 0, 0, time.UTC)
 			endDate = time.Date(h.Date.Year(), 1, 1, 0, 0, 0, 0, time.UTC)
+			prev_holiday = h.Date
 		}
 		ht := h.ToType()
 		holidays[ht.Date] = ht
 		holiday_list = append(holiday_list, ht)
 		endDate = h.Date
+		// http://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
+		// 3.その前日及び翌日が「国民の祝日」である日（「国民の祝日」でない日に限る。）は、休日とする。
+		if prev_holiday.AddDate(0, 0, 2).Equal(h.Date) {
+			ht_ := prev_holiday.AddDate(0, 0, 1)
+			ht = types.Holiday{Name: "", Type: 3, Date: ht_.Format("2006-01-02"), DayOfWeek: int(ht_.Weekday())}
+			holiday_list = append(holiday_list, ht)
+			holidays[ht.Date] = ht
+		}
+		prev_holiday = h.Date
 	}
 	endDate = time.Date(endDate.Year()+1, 1, 1, 0, 0, 0, 0, time.UTC)
 	// add sunday
